2e9f3a183216ec7b22a9554ec866de14686fc5c9
KashPitan <kashpitan@googlemail.com>
2025-04-30 17:00:26 +0100

    feat: add middleware for admin IP validation (#32)
    * feat: add middleware for admin IP validation and update env.example with ADMIN_IPS
    
    * remove console log for IP address in middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+const admin_ips: string[] =
+  process.env.ADMIN_IPS?.split(",").map((id) => id.trim()) || [];
+
+export async function middleware(request: NextRequest) {
+  if (process.env.ENVIRONMENT === "dev") {
+    return;
+  }
+
+  const headersList = headers();
+  const forwardedFor = (await headersList).get("x-forwarded-for");
+
+  const ipAddress = forwardedFor?.split(",").at(0) ?? undefined;
+
+  if (ipAddress && admin_ips.includes(ipAddress)) {
+    return;
+  }
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+  matcher: ["/admin/:path*", "/admin/(.*)"],
+};
