c1eac8518d376275f2ced60cf47939280ddfc33e
KashPitan <kashpitan@googlemail.com>
2025-05-06 23:03:21 +0100

    adjust to use absolute values

--- src/components/public/home-page/header.tsx
+++ src/components/public/home-page/header.tsx
@@ -3,13 +3,13 @@ import NavLink from "./nav-link";
 
 export default function Header() {
   return (
-    <header className="bg-racing-green/80 py-6 px-6 h-[9dvh] shadow-md backdrop-blur-md relative content-center">
+    <header className="bg-racing-green/80 py-6 px-6 h-[120px] shadow-md backdrop-blur-md relative content-center">
       <Image
         src={"/VestICLogo.png"}
         alt="vest-ic-logo-header"
         width="130"
         height="130"
-        className="absolute top-[2.75rem]"
+        className="absolute top-[50px]"
       />
       <nav className="container mx-auto flex justify-end ">
         {/* <div className="text-2xl font-bold text-pure-white">Vest IC</div> */}
