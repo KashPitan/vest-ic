0850a256ebec2767273611fb2b7e0ff34a25767d
KashPitan <kashpitan@googlemail.com>
2025-05-06 22:59:14 +0100

    added logo + increased header bar width

--- src/components/public/home-page/header.tsx
+++ src/components/public/home-page/header.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import NavLink from "./nav-link";
 
 export default function Header() {
   return (
-    <header className="bg-racing-green/80 py-6 px-6 shadow-md backdrop-blur-md">
-      <nav className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold text-pure-white">Vest IC</div>
+    <header className="bg-racing-green/80 py-6 px-6 h-[9dvh] shadow-md backdrop-blur-md relative content-center">
+      <Image
+        src={"/VestICLogo.png"}
+        alt="vest-ic-logo-header"
+        width="130"
+        height="130"
+        className="absolute top-[2.75rem]"
+      />
+      <nav className="container mx-auto flex justify-end ">
+        {/* <div className="text-2xl font-bold text-pure-white">Vest IC</div> */}
         <div className="flex gap-8">
           <NavLink href="/insights">Insights</NavLink>
           <NavLink href="/about">About</NavLink>
