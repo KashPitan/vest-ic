c41c7134038bbabda7157ae6bd5211452f2a420f
KashPitan <kashpitan@googlemail.com>
2025-05-06 21:12:55 +0100

    enhance: update BackgroundGraphic component with unoptimized and priority image loading

--- src/components/public/background-graphic/BackgroundGraphic.tsx
+++ src/components/public/background-graphic/BackgroundGraphic.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 import vestIcBackgroundGif from "./vest-ic-background-graphic.gif";
 
 const BackgroundGraphic = () => {
-  return <Image src={vestIcBackgroundGif} alt="background-graphic" fill />;
+  return (
+    <Image
+      src={vestIcBackgroundGif}
+      alt="background-graphic"
+      fill
+      unoptimized
+      priority
+    />
+  );
 };
 
 export default BackgroundGraphic;
