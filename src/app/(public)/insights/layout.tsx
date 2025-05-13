535ac89a7f5b2832e031264f18259c5912103899
KashPitan <kashpitan@googlemail.com>
2025-05-14 00:10:08 +0100

    remove placeholder background on insights search page

--- src/app/(public)/insights/layout.tsx
+++ src/app/(public)/insights/layout.tsx
@@ -17,16 +17,6 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       disableTransitionOnChange
     >
       <div className="relative flex flex-col min-h-screen">
-        {/* Background graphic */}
-        <div className="absolute inset-0 -z-10 opacity-10">
-          <Image
-            src="/landscape-placeholder.svg?height=1200&width=1200"
-            alt="Background graphic"
-            fill
-            className="object-cover"
-          />
-        </div>
-
         <main className="flex-1 flex items-start justify-center py-8">
           {children}
         </main>
