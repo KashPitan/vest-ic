8e35afe6169fa6e8fc12a43cdaa9e0bdf08b6a5b
Larry Burrows <lrryburrows@yahoo.co.uk>
2025-05-24 19:15:09 +0100

    lucia-auth implementation start

---
+++ src/db/schema/sessions.ts
@@ -0,0 +1,13 @@
+import { pgTable, uuid, timestamp } from "drizzle-orm/pg-core";
+import { users } from "./users";
+
+export const sessions = pgTable("session", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	userId: uuid("user_id")
+		.notNull()
+		.references(() => users.id),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date"
+	}).notNull()
+});
---
+++ src/db/schema/users.ts
@@ -0,0 +1,12 @@
+import { pgTable, uuid, text, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: text('username').unique(),
+  passwordHash: text('password'),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
