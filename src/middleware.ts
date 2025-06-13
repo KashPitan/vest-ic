85a76f512cbe274037993e9c2fdd1d43368eb4e0
KashPitan <kashpitan@googlemail.com>
2025-06-13 18:56:31 +0100

    added temp admin2 path to ip middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,5 +22,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/admin/(.*)"],
+  matcher: ["/admin/:path*", "/admin/(.*)", "/admin2/:path*", "/admin2/(.*)"],
 };
